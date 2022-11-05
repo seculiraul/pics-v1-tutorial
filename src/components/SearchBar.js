56222fc322528d404a02f454d2798424f37ccb56
Raul Seculi <raulseculi@Rauls-MacBook-Pro-2.local>
2022-11-05 13:11:40 +0200

    S7L91

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,11 +2,21 @@ import React from "react";
 
 class SearchBar extends React.Component {
 
+    state = {term: ''};
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        this.props.onSubmit(this.state.term);
+    }
+
     render() {
         return (
-            <div>
-                <form>
-                    <input type="text" />
+            <div className="ui segment">
+                <form className="ui form" onSubmit={this.onFormSubmit}>
+                    <div className="field">
+                        <label>Image field</label>
+                        <input type="text" value={this.state.term} onChange={e => this.setState({term: e.target.value})}/>
+                    </div>
                 </form>
             </div>
         )
