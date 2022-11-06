0e0bc4d78a281ab7080d5c0a7fdae686d7678cd8
Raul Seculi <raulseculi@Rauls-MacBook-Pro-2.local>
2022-11-06 22:18:28 +0200

    S9L105

---
+++ src/components/ImageList.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const ImageList = (props) => {
+   const images =  props.images.map(({description, id, urls}) => <img key={id} alt={description} src={urls.regular} />)
+    return <div>{images}</div>
+};
+
+export default ImageList
