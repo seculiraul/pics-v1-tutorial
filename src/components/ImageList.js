7bc2b3b90d19b7b6e51348a2f59bd6a4d3404e79
Raul Seculi <raulseculi@Rauls-MacBook-Pro-2.local>
2022-11-07 22:14:02 +0200

    S10L113

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,8 +1,10 @@
 import React from "react";
+import './ImageList.css';
+import ImageCard from "./ImageCard";
 
 const ImageList = (props) => {
-   const images =  props.images.map(({description, id, urls}) => <img key={id} alt={description} src={urls.regular} />)
-    return <div>{images}</div>
+   const images =  props.images.map((image) => <ImageCard key={image.id} image={image} />)
+    return <div className="image-list">{images}</div>
 };
 
 export default ImageList
